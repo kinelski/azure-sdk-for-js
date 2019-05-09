8980af9a7f80cba2aaf46cecf653b15da84f8a21
--show-origin <camaiaor@microsoft.com>
2019-05-09 14:28:36 -0700

    build succeeded. Browser tests not working

--- sdk/storage/storage-queue/rollup.test.config.js
+++ sdk/storage/storage-queue/rollup.test.config.js
@@ -1,6 +1,7 @@
 import multi from "rollup-plugin-multi-entry";
 import baseConfig from "./rollup.config";
 import sourcemaps from "rollup-plugin-sourcemaps";
+import json from "rollup-plugin-json";
 const [browser] = baseConfig;
 
 browser.input = ["dist-esm/test/*.js", "dist-esm/test/browser/*.js"];
@@ -8,6 +9,7 @@ browser.output.sourcemap = "inline";
 browser.output.file = "dist-test/index.browser.js";
 browser.plugins.unshift(multi());
 browser.plugins.unshift(sourcemaps());
+browser.plugins.unshift(json());
 browser.context = "null";
 
 export default [browser];
